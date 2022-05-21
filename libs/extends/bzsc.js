f4a2401d70a299583f5edf82447e9d5561174551
jhao <jhao@qq.com>
2022-05-21 15:00:20 +0800

    fux

---
+++ libs/extends/bzsc.js
@@ -0,0 +1,13 @@
+function getParams() {
+	var myparams = {};
+	myparams.center_url = "http://182.254.185.193/api";//devhttp://h5api.guoziyx.com
+	myparams.platform = "guoziyx";
+	myparams.isin = "0";
+	myparams.isin2 = "1";
+	myparams.ip = "182.254.185.193";//2931001197
+	myparams.port = "8000";
+	myparams.gmOpen = "999";
+	// myparams.ip = "119.29.82.91";//6217759362
+	// myparams.port = "9065";
+	return myparams;
+}
